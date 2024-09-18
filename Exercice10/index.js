fb59c65397342d6b206f9137b84099659a1f80cb
RFC06 <rfc06.dev@gmail.com>
2024-09-18 14:56:01 +0200

    test compilation et push ok

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,20 @@
 //votre code ici
 
 export default printNumbers
+//export default printNumbers
+
+
+function printNumbers(n) {
+    let result = '';
+    for (let i = 1; i <= n; i++) {
+        result += i + ' ';
+    }
+    return result.trim(); // Supprime l'espace final
+ }
+ 
+ 
+ export default printNumbers;
+ 
+ 
+ //export default printNumbers;
+ 
--- Exercice8/script.js
+++ Exercice8/script.js
@@ -1,3 +1,9 @@
-//Votre code ici
 
+//export default add;
+function add(a, b) {
+    return a + b;
+ }
+ 
+ 
  export default add;
+ 
